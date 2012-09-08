c8190bcd589cee2dc714ecfff21c426f1df5456d
Evgeniy Nikulin <nikulin.e@gmail.com>
2012-09-09 00:00:07 +0400

    Fix build pack script

--- build_pack.sh
+++ build_pack.sh
@@ -6,7 +6,8 @@
 rm -R /tmp/build_plugin/ru_geocoder
 mkdir /tmp/build_plugin/
 mkdir /tmp/build_plugin/ru_geocoder
-cp ./src/* /tmp/build_plugin/ru_geocoder
+cp -R ./src/* /tmp/build_plugin/ru_geocoder
+
 #Clean
 rm /tmp/build_plugin/ru_geocoder/*.pyc
 rm /tmp/build_plugin/ru_geocoder.zip
