177f3a1154e4baa5c4783b8722846af059ec5434
Evgeniy Nikulin <nikulin.e@gmail.com>
2012-09-25 01:46:30 +0400

    Prepare 0.3.2 release

--- src/__init__.py
+++ src/__init__.py
@@ -24,7 +24,7 @@ def name():
 def description():
     return "Geocode your csv files to shp (Russia only)"
 def version():
-    return "Version 0.3.1"
+    return "Version 0.3.2"
 def icon():
     return "icon.png"
 def qgisMinimumVersion():
