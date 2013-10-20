11c79e9ac429920f2e68b5c237b2a352f4ff77c7
yellow-sky <nikulin.e@gmail.com>
2013-10-20 23:02:13 +0400

    Implement #20. Added beta.osm.ru geocoder

--- src/beta_osm_ru_geocoder.py
+++ src/beta_osm_ru_geocoder.py
@@ -18,32 +18,8 @@
  *                                                                         *
  ***************************************************************************/
 """
-from exceptions import ValueError
-from osm_geocoder import OsmGeocoder
 from osm_ru_geocoder import OsmRuGeocoder
-from google_geocoder import GoogleGeocoder
-from beta_osm_ru_geocoder import BetaOsmRuGeocoder
-from yandex_geocoder import YandexGeocoder
 
 
-geocoders = {
-    'OSM': OsmGeocoder,
-    'OSM.RU': OsmRuGeocoder,
-    'BETA.OSM.RU': BetaOsmRuGeocoder,
-    'Google': GoogleGeocoder,
-    'Yandex': YandexGeocoder
-}
-
-
-class GeocoderFactory():
-
-    @staticmethod
-    def get_geocoder(geocoder_name):
-        if geocoder_name in geocoders.keys():
-            return geocoders[unicode(geocoder_name)]()
-        else:
-            raise ValueError('Unknown geocoder name')
-
-    @staticmethod
-    def get_geocoders_names():
-        return geocoders.keys()
+class BetaOsmRuGeocoder(OsmRuGeocoder):
+    url = 'http://beta.openstreetmap.ru/api/search?q='
